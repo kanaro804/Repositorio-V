1e7fb2c45713112e55ddd8a85ff61506b25f2e11
kanaro804 <kanaro804@gmail.com>
2021-06-16 20:48:28 -0600

    Create Practica 2.R

---
+++ Practica 2.R
@@ -0,0 +1,10 @@
+Practica #2
+
+#1. Genere un dataframe
+
+
+#2
+
+results=c("A","A", "B", "B", "B", "B", "B","I","I","I","R","I","I","I","R","R",
+          "R","R","R","R","R","R","R","R","R", "M", "M", "M", "M", "M", "M",
+          "M", "M", "M", "M", "M", "M", "M","A","A")
